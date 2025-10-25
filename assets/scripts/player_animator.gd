36baf4efdb10870d7148c1f8d3f61868fc2c7e1d
Sarvesh Neelraj <sarveshneel@gmail.com>
2025-10-25 19:32:25 +0400

    animations work now

--- assets/scripts/player_animator.gd
+++ assets/scripts/player_animator.gd
@@ -5,7 +5,17 @@ extends Node2D
 @export var sprite : Sprite2D
 
 func _process(delta):
+	#for flipping
 	if player_controller.direction == 1:
 		sprite.flip_h = false
 	elif player_controller.direction == -1:
 		sprite.flip_h = true
+	
+	#movement animation
+	if abs(player_controller.velocity.x) > 0.0:
+		animation_player.play("movement")
+	#idle animation
+	else:
+		animation_player.play("idle")
+	if abs(player_controller.velocity.y) < 0.0:
+		animation_player.play("jump")
