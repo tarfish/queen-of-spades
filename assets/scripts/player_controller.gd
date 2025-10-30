8099a195cdc32e41e3c4278ad98eb1d2fcecef2d
Sarvesh Neelraj <sarveshneel@gmail.com>
2025-10-30 16:53:45 +0400

    added jump buffering

--- assets/scripts/player_controller.gd
+++ assets/scripts/player_controller.gd
@@ -3,10 +3,13 @@ class_name PlayerController
 
 @export var speed = 10.0
 @export var jump_power = 10.0
+@export var Jump_Buffer_Timer: float = 0.1
 
 var speed_multiplier = 30.0
 var jump_multiplier = -30.0
 var direction = 0
+var Jump_Buffer:bool = false
+var Jump_Available:bool = true
 
 
 #const SPEED = 300.0
@@ -17,10 +20,19 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else:
+		Jump_Available = true
+		if Jump_Buffer:
+			Jump()
+			Jump_Buffer = false
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_power * jump_multiplier
+		if Jump_Available:
+			Jump()
+		else:
+			Jump_Buffer = true
+			get_tree().create_timer(Jump_Buffer_Timer).timeout.connect(on_jump_buffer_timeout)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -31,3 +43,10 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, speed * speed_multiplier)
 
 	move_and_slide()
+
+func Jump()->void:
+	velocity.y = jump_power * jump_multiplier
+	Jump_Available = false
+	
+func on_jump_buffer_timeout()->void:
+	Jump_Buffer = false
