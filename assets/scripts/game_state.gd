2b6abb84f9db308b5401636622883b62b4dc7921
Sarvesh Neelraj <sarveshneel@gmail.com>
2026-01-26 19:28:08 +0400

    finished portal script

---
+++ assets/scripts/game_state.gd
@@ -0,0 +1,2 @@
+extends Node
+var current_area := 1
--- assets/scripts/portal.gd
+++ assets/scripts/portal.gd
@@ -1,25 +1,32 @@
 extends Area2D
 class_name Portal
 
-@export var sprite : Sprite2D
+@export var sprite: Sprite2D
 @export var rotation_speed: float = 2.0
 
-var is_open = false
+var area_path := "res://assets/scenes/areas/"
+var is_open := false
 
 func _ready():
 	close()
+	body_entered.connect(_on_body_entered)
 
 func open():
 	is_open = true
 	sprite.region_rect.position.x = 32
-	
+
 func close():
 	is_open = false
 	sprite.region_rect.position.x = 0
 
-func _on_area_2d_body_entered(body):
-	if is_open && body.is_in_group("player"):
-		pass
-
 func _process(delta: float) -> void:
 	rotation += rotation_speed * delta
+
+func _on_body_entered(body: Node2D) -> void:
+	if is_open and body.is_in_group("player"):
+		next_level()
+
+func next_level():
+	GameState.current_area += 1
+	var full_path = area_path + "area_" + str(GameState.current_area) + ".tscn"
+	get_tree().change_scene_to_file(full_path)
