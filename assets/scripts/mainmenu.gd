16548b29c1bd5bbe2d6cfbe7ef3fa971793c3595
Sarvesh Neelraj <sarveshneel@gmail.com>
2026-01-30 21:37:30 +0400

    more main menu work

---
+++ assets/scripts/mainmenu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_startbutton_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quitbutton_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_optionsbutton_pressed() -> void:
+	pass # Replace with function body.
