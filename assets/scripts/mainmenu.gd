177096a8aaeafa37444aa0117d53a5501c9fed88
Sarvesh Neelraj <sarveshneel@gmail.com>
2026-01-31 08:09:08 +0400

    menu works except options

--- assets/scripts/mainmenu.gd
+++ assets/scripts/mainmenu.gd
@@ -2,12 +2,12 @@ extends Control
 
 
 func _on_startbutton_pressed() -> void:
-	pass # Replace with function body.
-
+	print ("game started")
+	get_tree().change_scene_to_file("res://assets/scenes/areas/area_1.tscn")
 
 func _on_quitbutton_pressed() -> void:
-	pass # Replace with function body.
-
+	print ("game quit")
+	get_tree().quit()
 
 func _on_optionsbutton_pressed() -> void:
-	pass # Replace with function body.
+	print ("opened options")
