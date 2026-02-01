d083fe577771b08aeb01b7e8a9c9dbf038904636
Sarvesh Neelraj <sarveshneel@gmail.com>
2026-02-01 10:45:56 +0400

    made better music

--- assets/scripts/mainmenu.gd
+++ assets/scripts/mainmenu.gd
@@ -1,13 +1,22 @@
 extends Control
 
+@onready var buttons: VBoxContainer = $buttons
+@onready var options: Panel = $options
+
+func _ready() -> void:
+	options.visible = false
+	buttons.visible = true
 
 func _on_startbutton_pressed() -> void:
-	print ("game started")
 	get_tree().change_scene_to_file("res://assets/scenes/areas/area_1.tscn")
 
 func _on_quitbutton_pressed() -> void:
-	print ("game quit")
 	get_tree().quit()
 
 func _on_optionsbutton_pressed() -> void:
-	print ("opened options")
+	buttons.visible = false
+	options.visible = true
+
+func _on_optionsquitbutton_pressed() -> void:
+	buttons.visible = true
+	options.visible = false
