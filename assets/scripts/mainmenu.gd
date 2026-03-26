f6290388add52103b11bdff12928091e8c2527da
Sarvesh Neelraj <sarveshneel@gmail.com>
2026-03-26 11:06:30 +0400

    added a tutorial

--- assets/scripts/mainmenu.gd
+++ assets/scripts/mainmenu.gd
@@ -2,10 +2,12 @@ extends Control
 
 @onready var buttons: VBoxContainer = $buttons
 @onready var options: Panel = $options
+@onready var tutorial = $tutorial
 
 func _ready() -> void:
 	options.visible = false
 	buttons.visible = true
+	tutorial.visible = false
 
 func _on_startbutton_pressed() -> void:
 	get_tree().change_scene_to_file("res://assets/scenes/areas/area_1.tscn")
@@ -16,11 +18,26 @@ func _on_quitbutton_pressed() -> void:
 func _on_optionsbutton_pressed() -> void:
 	buttons.visible = false
 	options.visible = true
+	tutorial.visible = false
 
 func _on_optionsquitbutton_pressed() -> void:
 	buttons.visible = true
 	options.visible = false
+	tutorial.visible = false
 
 
 func _on_musicvolume_value_changed(value: float) -> void:
 	pass
+
+
+func _on_tutorialbutton_pressed() -> void:
+	buttons.visible = false
+	options.visible = false
+	tutorial.visible = true
+	
+
+
+func _on_tutorialquitbutton_pressed() -> void:
+	tutorial.visible = false
+	options.visible = false
+	buttons.visible = true
